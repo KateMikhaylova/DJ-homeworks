227430ed6beb6e8b4fd6a69c0a8b5743f22349f8
Kate Mikhaylova <kate.mikhaylova@mail.ru>
2022-10-20 20:00:49 +0300

    Homework 5 added

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, db_column='sensor', on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='measurement/measurements', null=True, blank=True)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,33 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorDetailSerializer, MeasurementSerializer, SensorSerializer
+
+
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request, *args, **kwargs):
+        if request.data.get('sensor') is None or request.data.get('temperature') is None:
+            return Response({'error': "need to receive 'sensor' and 'temperature'"})
+        try:
+            sensor = Sensor.objects.get(id=request.data.get('sensor'))
+            m = Measurement.objects.create(temperature=request.data.get('temperature'),
+                                           sensor=sensor,
+                                           photo=request.data.get('photo'))
+            return Response({'sensor': m.sensor.id, 'temperature': m.temperature, 'created_at': m.created_at})
+        except ObjectDoesNotExist:
+            return Response({'error': 'indicated sensor is absent in database'})
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
